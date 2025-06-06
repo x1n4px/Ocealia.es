26dbaf98b1ec2f3ecbe045cd991dcf0c6d6e896e
x1n4px <silosabia@gmail.com>
2025-06-06 17:14:29 +0200

    act

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 import Home from './pages/home'
 import FishPage from './pages/fish'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
       <Routes>
